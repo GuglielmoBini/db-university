f850e0700ae98a4eea6a7c7b18112e0bf9b785e1
GuglielmoBini <guglielmobini@gmail.com>
2023-02-15 16:07:03 +0100

    join es 2

--- tracce/tracce Query con JOIN.sql
+++ tracce/tracce Query con JOIN.sql
@@ -1,10 +1,18 @@
 -- -------------------------------------------- QUERY CON JOIN
 
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-SELECT `students`.`name`, `students`.`surname`, `students`.`degree_id` FROM `students` JOIN `degrees` ON `degrees`.`id` = `students`.`degree_id` WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+SELECT `students`.`name`, `students`.`surname`, `students`.`degree_id` 
+FROM `students` 
+JOIN `degrees` 
+ON `degrees`.`id` = `students`.`degree_id` 
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-
+SELECT `degrees`.`id`, `degrees`.`name`, `degrees`.`level`, `degrees`.`department_id` 
+FROM `degrees` 
+JOIN `departments` 
+ON `departments`.`id` = `degrees`.`department_id` 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
